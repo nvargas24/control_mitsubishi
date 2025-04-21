e715483a84fa6b43a4d041f6573fac53cf8adf07
NEVargas <nahuvargas24@gmail.com>
2025-04-21 12:53:55 -0300

    Refactor extract_xlsx function to remove debug prints and add search_serie function for filtering by serial number

--- main.py
+++ main.py
@@ -15,14 +15,8 @@ def extract_xlsx():
     wb = load_workbook(URL)
     ws = wb.active
 
-    # Detecta si hay filas combinadas
-    for merge in ws.merged_cells.ranges:
-        print(f"Celdas combinadas: {merge}")
-
     try:
         df_test = pd.read_excel(URL, sheet_name=ws.title, header=None)
-        print(df_test)
-        print(df_test.info())
     except Exception as e:
         print(f"Error al cargar el archivo: {e}")
 
@@ -58,8 +52,19 @@ def extract_xlsx():
     df_clean = df.dropna(subset=["Num"])
     df_clean = df_clean.drop("Num", axis=1)
 
+    df_clean = df_clean.reset_index(drop=True)
+
     return df_clean
+
+def search_serie(df, num_serie):
+    df_filter = df.copy()
+
+    df_filter = df_filter[df_filter["Número de serie"] == num_serie]
+
+    return df_filter
+
 
 if __name__ == "__main__":
     df = extract_xlsx()
+    df = search_serie(df, "DA30765")
     print(df)
